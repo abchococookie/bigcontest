aa1359ff7b5d4ea80cad3ae5f97fd7a9d89ca2de
Jihee HAN <143975468+abchococookie@users.noreply.github.com>
2024-10-18 19:48:14 +0900

    delete test tab

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ if update:
 
 # main page
 import BIGC_VIS_for_dashboard
-from df_generation_for_dashboard import gen_df_stay
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["STAY", "OD-IN", "OD-OUT", "OD-DIFF", "Test"])
 
@@ -84,9 +83,3 @@ with tab4:
     except Exception as e:
         logging.error("Error in STAY Visualization: %s", e)
         st.error("해당 날짜 및 시간에 해당하는 데이터가 없습니다.", icon="🚨")
-        
-with tab5:
-    st.header("Test")
-    st.info("dataframe 출력 테스트용 페이지입니다.", icon="ℹ️")
-    st.subheader("STAY")
-    st.table(gen_df_stay(st.session_state['day'], st.session_state['time']))
