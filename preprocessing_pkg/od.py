de344afba8ef5a374eee39d5938a250bdce7f665
Jihee HAN <143975468+abchococookie@users.noreply.github.com>
2024-11-10 19:50:00 +0900

    preprocessing with zip files

--- preprocessing_pkg/od.py
+++ preprocessing_pkg/od.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import glob
+import zipfile
 
-def od_in(date: int, end_time: str, dest: int, from_seoul: bool):
-    # 해당 날짜 csv 파일 읽어오기
-    csv_dir = glob.glob(f"data/**/od_{date}*.csv", recursive = True)
-    df = pd.read_csv(csv_dir[0])    # 하루치 데이터를 읽을 때에만 성립
+od_zip_sep = zipfile.ZipFile("data/od_sep.zip")
+od_zip_oct = zipfile.ZipFile("data/od_oct.zip")
+od_list = od_zip_sep.namelist() + od_zip_oct.namelist()
+
+def od_in(date: int, end_time: int, dest: int, from_seoul: bool):
+    # 해당 날짜 csv 파일 압축 풀고 읽기
+    if not glob.glob(f"data/**/od_{date}_1.csv", recursive=True):
+        target = [file for file in od_list if str(date) in file][0]
+        if "202309" in target:
+            od_zip_sep.extract(target, "data/od")
+        else:
+            od_zip_oct.extract(target, "data/od")
+        csv_dir = f"data/od/{target}"
+    else:
+        csv_dir = glob.glob(f"data/**/od_{date}_1.csv", recursive=True)[0]
+    
+    df = pd.read_csv(csv_dir)
     
     # 행정동 코드 문자열로 바꾸기 (시각화 때 merged_df 생성 위해)
     df["dest_hdong_cd"] = df["dest_hdong_cd"].astype(str)
@@ -16,7 +30,7 @@ def od_in(date: int, end_time: str, dest: int, from_seoul: bool):
     
     # dest_hdong_cd 필터링 & end_time 필터링
     df.loc[:, 'end_time'] = pd.to_datetime(df['end_time'], format='%H:%M').dt.hour 
-    df = df[(df["dest_hdong_cd"] == str(dest)) & (df["end_time"].astype("str") == end_time)]
+    df = df[(df["dest_hdong_cd"] == str(dest)) & (df["end_time"].astype("str") == str(end_time))]
     
     # 해당되는 모든 데이터의 이동인원 summation
     df = df.groupby("origin_hdong_cd")["od_cnts"].sum().sort_values(ascending=False).reset_index()
@@ -24,10 +38,16 @@ def od_in(date: int, end_time: str, dest: int, from_seoul: bool):
     return df
 
 
-def od_out(date: int, start_time: str, origin: int, to_seoul: bool):
-    # 해당 날짜 csv 파일 읽어오기
-    csv_dir = glob.glob(f"data/**/od_{date}*.csv", recursive = True)
-    df = pd.read_csv(csv_dir[0])    # 하루치 데이터를 읽을 때에만 성립
+def od_out(date: int, start_time: int, origin: int, to_seoul: bool):
+    # 해당 날짜 csv 파일 압축 풀고 읽기
+    if not glob.glob(f"data/**/od_{date}_1.csv", recursive=True):
+        target = [file for file in od_list if str(date) in file][0]
+        od_zip.extract(target, "data/od")
+        csv_dir = f"data/od/{target}"
+    else:
+        csv_dir = glob.glob(f"data/**/od_{date}_1.csv", recursive=True)[0]
+    
+    df = pd.read_csv(csv_dir)
     
     # 행정동 코드 문자열로 바꾸기 (시각화 때 merged_df 생성 위해)
     df["dest_hdong_cd"] = df["dest_hdong_cd"].astype(str)
@@ -39,7 +59,7 @@ def od_out(date: int, start_time: str, origin: int, to_seoul: bool):
         
     # origin_hdong_cd 필터링 & start_time 필터링
     df.loc[:, 'start_time'] = pd.to_datetime(df['start_time'], format='%H:%M').dt.hour 
-    df = df[(df["origin_hdong_cd"] == str(origin)) & (df["start_time"].astype("str") == start_time)]
+    df = df[(df["origin_hdong_cd"] == str(origin)) & (df["start_time"].astype("str") == str(start_time))]
     
     # 해당되는 모든 데이터의 이동인원 summation
     df = df.groupby("dest_hdong_cd")["od_cnts"].sum().sort_values(ascending=False).reset_index()
--- preprocessing_pkg/stay.py
+++ preprocessing_pkg/stay.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import glob
+import zipfile
 
-def stay(day, time):
-    csv_dir = glob.glob(f"data/**/stay_{day}_1.csv", recursive = True)
-    data = pd.read_csv(csv_dir[0])
+stay_zip = zipfile.ZipFile("data/stay.zip")
+stay_list = stay_zip.namelist()
+
+def stay(date: int, time: int):
+    # 해당 날짜 csv 파일 압축 풀고 읽기
+    if not glob.glob(f"data/**/stay_{date}_1.csv", recursive=True):
+        target = [file for file in stay_list if str(date) in file][0]
+        stay_zip.extract(target, "data/stay")
+        csv_dir = f"data/stay/{target}"
+    else:
+        csv_dir = glob.glob(f"data/**/stay_{date}_1.csv", recursive=True)[0]
+    
+    data = pd.read_csv(csv_dir)
     
     # 서울시에 해당하는 행만 필터링
-    data = data[(data['hdong_cd'] >= 1100000000) & (data['hdong_cd'] <= 1174070000)] 
+    data["hdong_cd"] = data["hdong_cd"].astype("str")
+    data = data[data["hdong_cd"].str.startswith("11")]
     
     # 시간 형식을 변환 (예: 13:00 -> 13)
     data.loc[:, 'time'] = pd.to_datetime(data['time'], format='%H:%M').dt.hour 
     
     # input 시간에 해당하는 데이터만 필터링
-    data = data[data['time'].astype("str") == time]
+    data = data[data['time'].astype("str") == str(time)]
     
     # 행정동 코드와 시간을 기준으로 그룹화하고, 체류인원 합산
     data_grouped = data.groupby(['hdong_cd', 'time'], as_index=False).agg({'stay_cnts': 'sum'})
